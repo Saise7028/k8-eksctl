5ccfc108c0dddf14261f7d349df9b354fc834656
suresh <saise7028@gmail.com>
2024-10-25 18:38:20 +0530

    kubernetes

--- script.sh
+++ script.sh
@@ -7,7 +7,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "installing the script with root previliges"
-    echo "Resizeing the Disk Volume"
+    exit 1
+fi    
+echo "Resizeing the Disk Volume"
 df -hT
 lsblk
 growpart /dev/xvda 4  # t2.micro is /dev/xvda and t3.micro is /dev/nvme0n1 4
@@ -16,5 +18,4 @@ lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 df -hT
-    exit 1
-fi    
+
