0ec08dc86a8b119e2967400d26a3d767771c201a
suresh <saise7028@gmail.com>
2024-10-25 19:41:02 +0530

    kubernetes

--- script.sh
+++ script.sh
@@ -25,12 +25,45 @@ xfs_growfs /var
 df -hT
 
 # Installing Docker
+
 echo -e "$G Docker Installation $N"
+
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
-echo -e "$G Logout and Login again $N"
+echo -e "$Y Logout and Login again $N"
+
+echo -e "$R Docker Installation Successfully Completed $N"
+
+# Kubectl Installation 
+
+echo -e "$G Kubectl Installation $N"
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+kubectl version
+
+echo -e "$G Docker Installation Successfully Completed $N"
+
+# eksctl Installation
+
+echo -e "$Y Docker Installation Successfully Completed $N"
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+mv /tmp/eksctl /usr/local/bin
+
+echo -e "$G Docker Installation Successfully Completed $N"
 
