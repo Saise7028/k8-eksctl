7943ef6ea816624d63733e66b87ab0356afa047b
suresh <saise7028@gmail.com>
2024-10-27 17:50:47 +0530

    kubernetes

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "work_station" {
+resource "aws_instance" "station" {
   ami                    = "ami-09c813fb71547fc4f"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["vpc-0b8195d98ce48090a"]
   tags = {
-    Name = "workstation"
+    Name = "station"
   }
 
 }
 
-resource "aws_security_group" "allow_all_work_station" {
+resource "aws_security_group" "allow_all_station" {
   name        = "Allow_All"
   description = "Allow port number 22 for ssh Access"
 
@@ -31,7 +31,7 @@ resource "aws_security_group" "allow_all_work_station" {
   }
 
   tags = {
-    Name = "workstation"
+    Name = "station"
   }
 
 
